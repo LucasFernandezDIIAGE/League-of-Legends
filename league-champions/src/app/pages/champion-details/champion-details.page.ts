ef8d1073483dac119c9279369e05aea4bb9b9ae8
LucasFernandez <lucas.fernandez14102002@gmail.com>
2024-10-29 13:25:31 +0100

    routing fonctionnel et récupération de l'id du champion

--- league-champions/src/app/pages/champion-details/champion-details.page.ts
+++ league-champions/src/app/pages/champion-details/champion-details.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ChampionsService } from 'src/Services/champions-services.service';
+import { Champion } from 'src/Domain/Champions/Champion.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-champion-details',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChampionDetailsPage implements OnInit {
 
-  constructor() { }
+  champion: Champion = {
+    id : '',
+    nom: '',
+    titre: '',
+    categories: [],
+    skins: [],
+    stats: undefined
+  }
+  championId: string = ''
+
+  constructor(
+    private championsService: ChampionsService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
+    this.route.paramMap.subscribe((params)=>{
+      this.championId = params.get('id')!
+    })
+  }
+
+  async loadChampion(id: string){
+    this.championsService.getChampionById(id).subscribe((champion)=>{
+      this.champion = champion
+    })
   }
 
 }
